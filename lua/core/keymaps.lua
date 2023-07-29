39b246c6dd8fd5525f89d03f7c7b88de06071695
Kohei <nisijinhokuryou@gmail.com>
2023-07-29 16:41:24 +0900

    Add keymaps.

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,30 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+--local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+
+-- New tab
+keymap("n", "te", ":tabedit", opts)
+-- 新しいタブを一番右に作る
+keymap("n", "gn", ":tabnew<Return>", opts)
+-- move tab
+keymap("n", "gh", "gT", opts)
+keymap("n", "gl", "gt", opts)
+
+-- Split window
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
+
+-- Select all
+keymap("n", "<C-a>", "gg<S-v>G", opts)
+
+-- Do not yank with x
+keymap("n", "x", '"_x', opts)
+
+-- Delete a word backwards
+keymap("n", "dw", 'vb"_d', opts)
+
+-- 行の端に行く
+keymap("n", "<Space>h", "^", opts)
+keymap("n", "<Space>l", "$", opts)
